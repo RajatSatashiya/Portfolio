807e38bc1f8672d7c1b5b2ffa6756546fa861314
Rajat Satashiya <kshri4398@gmail.com>
2022-06-01 22:56:47 +0530

    portfolio complete

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,52 +4,24 @@ import "./footer.css";
 function footer() {
   return (
     <footer>
-      <div className="footerUp">
-        <div>
-          <div className="footHead">Quick Links</div>
-          <div className="list">Rent a room</div>
-          <div className="list">Find roommates</div>
-          <div className="list">
-            <a href="how.html">How it works</a>
-          </div>
-        </div>
-        <div>
-          <div className="footHead">About</div>
-          <div className="list">Our Story</div>
-          <div className="list">Team</div>
-        </div>
-        <div>
-          <div className="footHead">Help</div>
-          <div className="list">Frequently Asked Questions</div>
-          <div className="list">Blog</div>
-          <div className="list">Contact Us</div>
-        </div>
-        <div>
-          <div className="footHead">Legal</div>
-          <div className="list">Terms and Conditions</div>
-          <div className="list">Privacy Policy</div>
-        </div>
-        <div>
-          <div className="footHead">Contact</div>
-          <div className="list">roommatefinder@gmail.com</div>
-        </div>
+      <div className="socials">
+        <a href="mailto:rajat09satashiya@gmail.com">
+          <i className="fas fa-envelope mail"></i>
+        </a>
+        <a href="https://www.instagram.com/mr__satashiya/">
+          <i className="fab fa-instagram insta"></i>
+        </a>
+        <a href="https://github.com/RajatSatashiya">
+          <i className="fab fa-github gb"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/rajatsatashiya/">
+          <i className="fab fa-linkedin ld"></i>
+        </a>
       </div>
 
-      <div class="footerDown">
-        <div class="copy">
-          Copyright © 2020-2021 Roommate Finder. All rights reserved
-        </div>
-        <div class="socials">
-          <a href="https://www.instagram.com/roomatefinder/">
-            <i class="fab fa-instagram insta"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-facebook fb"></i>
-          </a>
-          <a href="https://www.linkedin.com/company/rmt-finder/about/">
-            <i class="fab fa-linkedin ld"></i>
-          </a>
-        </div>
+      <div className="ending">
+        <div className="salutation">Hecho Con Amor Por,</div>
+        <div className="myname">Rajat Satashiya</div>
       </div>
     </footer>
   );
